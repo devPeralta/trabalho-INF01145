85edbfdda668a3adc99a3934e864dcc0c1a3bf1d
Pablo Peralta <pdperalta@inf.ufrgs.br>
2024-08-05 18:55:15 -0300

    entidades passageiros e motoristas adicionadas
    essas duas entidades são heranças da entidade usuarios

--- blablacar.sql
+++ blablacar.sql
@@ -1,20 +1,35 @@
-CREATE TABLE usuarios (
-                          id SERIAL PRIMARY KEY,        -- ID único e auto-incremental para cada usuário
-                          nome VARCHAR(100) NOT NULL,   -- Nome completo do usuário, não nulo
-                          email VARCHAR(50) UNIQUE NOT NULL, -- E-mail do usuário, único e não nulo
-                          telefone VARCHAR(15),         -- Telefone do usuário, opcional
-                          sobre TEXT,                   -- Descrição sobre o usuário, opcional
-                          dataRegistro TIMESTAMP DEFAULT CURRENT_TIMESTAMP -- Data e hora do registro, padrão é o momento atual
+CREATE TABLE usuarios
+(
+    id           SERIAL PRIMARY KEY,                 -- ID único e auto-incremental para cada usuário
+    nome         VARCHAR(100)       NOT NULL,        -- Nome completo do usuário, não nulo
+    email        VARCHAR(50) UNIQUE NOT NULL,        -- E-mail do usuário, único e não nulo
+    telefone     VARCHAR(15),                        -- Telefone do usuário, opcional
+    sobre        TEXT,                               -- Descrição sobre o usuário, opcional
+    dataRegistro TIMESTAMP DEFAULT CURRENT_TIMESTAMP -- Data e hora do registro, padrão é o momento atual
 );
 
-CREATE TABLE avaliacoes (
-                                                      id SERIAL PRIMARY KEY,
-                                                      mensagem TEXT NOT NULL,
-                                                      nota INTEGER CHECK (nota >= 1 AND nota <= 10),
-                                                      data TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                                                      avaliador_id INTEGER NOT NULL,
-                                                      avaliado_id INTEGER NOT NULL,
-                                                      FOREIGN KEY (avaliador_id) REFERENCES usuarios(id),
-                                                      FOREIGN KEY (avaliado_id) REFERENCES usuarios(id)
+CREATE TABLE avaliacoes
+(
+    id           SERIAL PRIMARY KEY,
+    mensagem     TEXT    NOT NULL,
+    nota         INT CHECK (nota >= 1 AND nota <= 5),
+    data         TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    avaliadorID INT NOT NULL,
+    avaliadoID  INT NOT NULL,
+    FOREIGN KEY (avaliadorID) REFERENCES usuarios (id),
+    FOREIGN KEY (avaliadoID) REFERENCES usuarios (id)
+);
+
+CREATE TABLE motoristas
+(
+    id      INT PRIMARY KEY REFERENCES usuarios (id),
+    quantidadeCaronas INT NOT NULL
+
+);
+
+CREATE TABLE passageiros
+(
+    id INT PRIMARY KEY REFERENCES usuarios (id),
+    localizacaoAtual VARCHAR(100) NOT NULL
 );
 
